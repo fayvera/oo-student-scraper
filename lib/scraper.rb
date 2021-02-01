3e518c1b571b97d34cb4a8515816af3e4123aac3
Fay Vera <fvera.cavalcante@gmail.com>
2021-01-31 23:55:24 -0500

    commit tree c7d43b8a761e2370aa344e8088da17159e74927a

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,47 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
-  end
+    index_page = Nokogiri::HTML(open(index_url))
+      students = []
+      # index_page.css("div.roster-cards-container").each do |card|
+      #   card.css(".student-card a").each do |student|
+      #     student_name = student.css('.student-name').text
+      #     student_location = student.css('.student-location').text
+      #     student_profile_url = "#{student.attr('href')}"
+      #     students << {name: student_name, location: student_location, profile_url: student_profile_url}
+
+      index_page.css("div.roster-cards-container").each do |card|
+        card.css(".student-card a").each do |student|
+          student_profile_link = "#{student.attr('href')}"
+          student_location = student.css('.student-location').text
+          student_name = student.css('.student-name').text
+          students << {name: student_name, location: student_location, profile_url: student_profile_link}
+        end
+      end
+      students
+    end
 
   def self.scrape_profile_page(profile_url)
-    
+      student = {}
+      profile = Nokogiri::HTML(open(profile_url))
+      # binding.pry
+      links = profile.css(".social-icon-container").children.css("a").map{ |l| l.attribute('href').value}
+      links.each do |link|  
+        if link.include?("linkedin")
+          student[:linkedin] = link
+        elsif link.include?("twitter")
+          student[:twitter] = link
+        elsif link.include?("github")
+          student[:github] = link
+        else
+          student[:blog] = link
+        # binding.pry
+        end
+      end
+      student[:profile_quote] = profile.css(".profile-quote").text if profile.css(".profile-quote")
+      # binding.pry
+      student[:bio] = profile.css("div.bio-content.content-holder div.description-holder p").text if student[:bio] = profile.css("div.bio-content.content-holder div.description-holder p")
+      student
   end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,30 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send(("#{key}="), value)   
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    # binding.pry
+    students_array.each do |student_hash|
+      Student.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    # binding.pry
+    attributes_hash.each do |attribute, value|
+      self.send("#{attribute}=", value)
+    end
+    self
   end
 
   def self.all
-    
+    @@all
   end
 end
 
